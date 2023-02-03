c5d6e69ba638709eb7a08b47d4d2c02548c09936
Daniel <115842725+DanielDeveloperBR@users.noreply.github.com>
2023-02-03 19:08:01 -0300

    upgrade 3.0
    atualizar nova

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ $ git clone https://danieldeveloperbr.github.io/Projeto-Integrador/
 
 This project was developed with the following technologies:
 
-- HTML, CSS, PHP and Bootstrap.
+- HTML, CSS and Bootstrap
 <!-- - [Expo][expo] -->
 
 # Features
--- php/login.php
+++ php/login.php
@@ -45,7 +45,7 @@ if (isset($_POST['email'])) {
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/user.css">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
 <form action="" method="post" id="modal" class="animar">     
